7a67e5a2b03e006cf4678580741adfbe94318b9d
Daniel Sundermeier <daniel@hof-sundermeier.de>
2020-09-19 13:09:43 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,6 @@ php artisan queue:restart
 composer install  > /dev/null 2>&1 & echo $! #--no-interaction --no-dev --prefer-dist
 npm install > /dev/null 2>&1 & echo $!
 npm run production > /dev/null 2>&1 & echo $!
-php artisan lang:generate --quiet
 php artisan optimize
 
 echo "Deployed"
