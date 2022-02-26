3b8dd9d5b2388efa0951abd352fac28ffa3f3e8b
Daniel Sundermeier <daniel@hof-sundermeier.de>
2022-02-26 05:37:11 +0100

    Arbeitszeit: Start und Ende mit Pause

--- resources/views/staff/workingtime/show.blade.php
+++ resources/views/staff/workingtime/show.blade.php
@@ -5,6 +5,8 @@
     <div class="d-flex align-items-center flex-column">
         <h1>Arbeitszeit</h1>
 
+        <h2>{{ $model->name }}</h2>
+
         <partner-staff-workingtime-show :selected-staff-id="{{ $model->id }}" :model="{{ json_encode($model) }}"></partner-staff-workingtime-show>
     </div>
 
